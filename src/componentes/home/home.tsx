68cde6e0bbea8f5be759067154ec7d77b218cfcb
AndersonMSilva91 <andersonm.das.91@gmail.com>
2022-11-24 11:40:52 -0300

    home

---
+++ src/componentes/home/home.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './home.css';
+
+function Home(){
+    return(
+
+        <>
+        <h1>Home</h1>
+        <img src="" alt="" />
+        </>
+
+    );
+}
+
+export default Home
--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,35 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ThemeProvider } from '@material-ui/core';
+import { createTheme } from '@material-ui/core/styles';
+import { brown, yellow } from '@material-ui/core/colors';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: brown[500],
+      main: yellow[500],
+      dark: '#002884',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#ff7961',
+      main: '#f44336',
+      dark: '#ba000d',
+      contrastText: '#000',
+    },
+  },
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <ThemeProvider theme={(theme)}>
     <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
